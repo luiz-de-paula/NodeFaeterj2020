87989dfea73305a0712f02d30245bb982401a386
Luiz-de-Paula <eletronica.do.lui@gmai.com>
2020-10-06 02:34:28 -0300

    copia index do professor

--- index.js
+++ index.js
@@ -1,77 +1,96 @@
-const express = require ('express');
+const express = require('express');
 
-const server = express();
-server.use(express.json());
+const server = express(); // A variável server recebe express(),
+                          // os parenteses indicam que o express exporta uma função,
+                          // Ou seja, estamos chamando a função do express. 
 
-const users = ['Luiz', 'Rubens', 'Marina'];
+server.use(express.json()); // o server é a instância do express, o use é um plugin já importado 
+                            // que estamos adicionando pra ele (express) e passamos pra ele o
+                            // express.json()
 
-//Midleeware global
-// server.use((req, res, next) => {
-//   console.time('request'),
-//   console.log(`Metodo: ${req.method}, URL ${req.url}`);
+const users = ['Rubens', 'Eliete', 'Marina'];
 
-//  next();
-//  console.timeEnd('request');
+// Middleware Global
+server.use((req, res, next) => {
+  console.time('request');
+  console.log(`Metodo: ${req.method}, URL ${req.url}`);
 
-// });
-// // Middleware Local
-// function checkUserNameExist(req, res, next) {
-//   if (!req.body.name){
-//     return res.status(400).json({error: 'User name' is requered})
-//   }
+  next();
 
-     
-//   return next();
-// }
+  console.timeEnd('request');
+});
 
-// function checkUserInArray(req, res, next) {
-//   const user = users [req.params.index];
-//   if (!user) {
-//     return res.status(400).json({ error: "user does not exist. "});
-//   }
-//   req.user = user;  
-//   return next();
-// }
+// Middleware Local
+function checkUserNameExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'User name is required!'});
+  } 
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+
+  if (!user) {
+    return res.status(400).json({ error: "User does not exists!"});
+  }
+
+  req.user = user;
+
+  return next();
+
+}
+
+// CRUD - CREATE, READ, UPDATEe DELETE
+/**
+ * ROTAS get, post, put e dele a seguir 
+ */
+
+// Consultar - Listar todos os usuario
 server.get('/users', (req, res) => {
-  return res.json (users);
+  return res.json(users); // Lista todos os usuários 
 });
 
-server.get('/users/:index', (req, res) => {
-  const {index} = req.params;
-  return res.json(users[index]);
+// Consultar - Listar um usuario
+server.get('/users/:index', checkUserInArray, (req, res) => {
+  // Aqui exibiremos o usuário referente ao id capturado na rota
+  return res.json({message : `Exibindo o usuário ` + req.user});
 });
 
-server.post ("/users", (req, res) => {
-  const {name} = req.body;
-  users.push(name);
-  return res.json(users); 
+// Create
+// Incluir - Cadastrar usuario
+server.post('/users', checkUserNameExists, (req, res) => {
+  const { name } = req.body;
 
-});
-
-
-server.put("/users/:index", (req, res) => {
-  const {index} = req.params;
-  const {name} = req.body;
-
-  users[index] = name;
+  users.push(name); // Insere o usuário passado no corpo da requisição no vetor
 
   return res.json(users);
 });
 
+// Update
+// Alterar - Editar um usuario
+server.put('/users/:index', checkUserNameExists, checkUserInArray, (req, res) => {
+    const { index } = req.params;
+    const { name }  = req.body;
+
+    users[index] = name; // Substitui no vetor o nome que está na posição do 
+                         // index passado pelo nome passado no corpo da requisição
+
+    return res.json(users);
+});
+
+// Delete
+// Excluir - Deletar um usuario
 server.delete("/users/:index", (req, res) => {
-  
-  const {index} = req.params;
-  users.splice(index, 1)
+  const { index } = req.params;
 
+  users.splice(index, 1); // O método splice percorre o vetor até o index e 
+                          // e exclui a partir daquela posição o número de
+                          // posições passada no segundo parâmetro.
 
-
-
-
-
-
-  return res.json(users);
+  return res.send();
 });
 
-
+// Porta utilizada pelo servidor
 server.listen(3000)
-
